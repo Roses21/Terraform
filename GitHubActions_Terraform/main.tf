4349104d3374c2b28952a00de01835be2fcab7aa
Roses21 <147015288+Roses21@users.noreply.github.com>
2024-11-08 16:51:59 +0700

    Update main.tf

--- GitHubActions_Terraform/main.tf
+++ GitHubActions_Terraform/main.tf
@@ -1,24 +1,40 @@
-terraform {
- required_providers {
-   aws = {
-     source = "hashicorp/aws"
-   }
- }
- 
- backend "s3" {
-   region = "ap-southeast-1"
-   key    = "terraform.tfstate"
- }
-}
- 
-provider "aws" {
- region = "ap-southeast-1"
-}
- 
-resource "aws_instance" "test_instance" {
- ami           = "ami-08f49baa317796afd"
- instance_type = "t2.micro"
- tags = {
-   Name = "test_instance"
- }
-}
+name: CI/CD Pipeline
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  terraform:
+    name: Terraform Apply
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Check out code
+        uses: actions/checkout@v2
+
+      - name: Set up Terraform
+        uses: hashicorp/setup-terraform@v2
+        with:
+          terraform_wrapper: false
+
+      - name: Terraform Init
+        run: terraform init
+        working-directory: ./terraform
+
+      - name: Terraform Format
+        run: terraform fmt -check
+        working-directory: ./terraform
+
+      - name: Terraform Plan
+        run: terraform plan
+        working-directory: ./terraform
+
+      - name: Terraform Apply
+        if: github.ref == 'refs/heads/main'
+        run: terraform apply -auto-approve
+        working-directory: ./terraform
+        env:
+          AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
+          AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
