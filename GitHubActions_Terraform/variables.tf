5113f8917437c6b136157b913635ea11f851f08b
Roses21 <147015288+Roses21@users.noreply.github.com>
2024-11-08 17:22:12 +0700

    Update variables.tf

--- GitHubActions_Terraform/variables.tf
+++ GitHubActions_Terraform/variables.tf
@@ -1,12 +1,1 @@
-variable "region" {
-  description = "The AWS region to deploy resources"
-  type        = string
-  default     = "ap-southeast-1"
-}
-
-variable "project_name" {
-  description = "Demo CI/CD AWS of the project"
-  type        = string
-  default     = "my_project_name" # Thêm giá trị mặc định
-}
 
