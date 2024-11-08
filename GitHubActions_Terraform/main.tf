ae83703416bcb7ce08d40a47d35325e9c7f29898
Roses21 <147015288+Roses21@users.noreply.github.com>
2024-11-08 17:21:50 +0700

    Update main.tf

--- GitHubActions_Terraform/main.tf
+++ GitHubActions_Terraform/main.tf
@@ -1,8 +1,10 @@
-resource "aws_instance" "demo_cicd" {
-  ami           = "ami-08f49baa317796afd " # Minux 2023 AMI
+resource "aws_instance" "terraform_cicd_demo" {
+  ami = "ami-08f49baa317796afd" # Amazon Linux 2023 AMI
   instance_type = "t2.micro"
-
+  availability_zone = "ap-southeast-1a"
+  key_name = "terraform_keypair"
+  vpc_security_group_ids = ["sg-03a4cc4d776b59709"]
   tags = {
-    Name = "${var.project_name}-instance"
+    Name = "terraform_instance_demo"
   }
 }
