8794e2931383b1cdeb499f06299b45c247b1e2f9
Roses21 <147015288+Roses21@users.noreply.github.com>
2024-11-08 16:56:21 +0700

    Update main.tf

--- GitHubActions_Terraform/main.tf
+++ GitHubActions_Terraform/main.tf
@@ -1,40 +1,8 @@
-name: CI/CD Pipeline
+resource "aws_instance" "demo_cicd" {
+  ami           = "ami-08f49baa317796afd " # Minux 2023 AMI
+  instance_type = "t2.micro"
 
-on:
-  push:
-    branches:
-      - main
-
-jobs:
-  terraform:
-    name: Terraform Apply
-    runs-on: ubuntu-latest
-
-    steps:
-      - name: Check out code
-        uses: actions/checkout@v2
-
-      - name: Set up Terraform
-        uses: hashicorp/setup-terraform@v2
-        with:
-          terraform_wrapper: false
-
-      - name: Terraform Init
-        run: terraform init
-        working-directory: ./terraform
-
-      - name: Terraform Format
-        run: terraform fmt -check
-        working-directory: ./terraform
-
-      - name: Terraform Plan
-        run: terraform plan
-        working-directory: ./terraform
-
-      - name: Terraform Apply
-        if: github.ref == 'refs/heads/main'
-        run: terraform apply -auto-approve
-        working-directory: ./terraform
-        env:
-          AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
-          AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  tags = {
+    Name = "${var.project_name}-instance"
+  }
+}
